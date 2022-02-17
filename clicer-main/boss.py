65b1690274751623ade03e48e5d460843683349e
abrikosmna <95037700+abrikosmna@users.noreply.github.com>
2022-02-17 21:33:29 +0700

    Add files via upload

--- clicer-main/boss.py
+++ clicer-main/boss.py
@@ -13,6 +13,9 @@ class Boss:
 
         self.picture = pygame.image.load('boss\\boss.jfif')
         self.pos = (100, 100)
+        self.boss_WIDTH = 220
+        self.boss_HEIGHT = 320
+        self.pos_boss = (220, 320)
 
     def draw_bloks(self, screen):
         screen.blit(self.blok_picture, self.blok_pos)
@@ -21,4 +24,23 @@ class Boss:
         return False
 
     def draw_boss(self, screen):
-        screen.blit(self.picture, self.pos)
+        screen.blit(self.picture, (300, 120))
+
+    def hp_boss(self, hp, screen):
+        f1 = pygame.font.Font(None, 36)
+        text1 = f1.render("HP  " + str(self.hp), True,
+                          (0, 0, 0))
+        screen.blit(text1, (360, 500))
+
+    def is_clicked_boss(self,pos_boss):
+        return self.pos_boss[0] < pos_boss[0] < self.pos_boss[0] + self.boss_WIDTH and \
+               self.pos_boss[1] < pos_boss[1] < self.pos_boss[1] + self.boss_HEIGHT
+
+    def atack(self, hp):
+        if self.hp == 0:
+            self.level += 1
+            self.aurum += self.level ** 2
+            print(12345)
+        if self.is_clicked_boss():
+            self.hp -= 1
+        return self.hp
--- clicer-main/main.py
+++ clicer-main/main.py
@@ -1,17 +1,25 @@
 import pygame
+from boss import Boss
 from boss_menu import Boss_menu
 from level_changer import LevelChanger
 
+
 class Main:
 
+
+
     def __init__(self):
+
         pygame.init()
         self.WINDOW_WIDHT = 600
         self.WINDOW_HEIGHT = 600
         self.screen = pygame.display.set_mode((self.WINDOW_WIDHT, self.WINDOW_HEIGHT))
         self.boss_menu = Boss_menu()
-        self.white = [255, 255, 255]
         self.active_boss_level = 1
+        self.boss = Boss(self.active_boss_level,100)
+        self.white = [255, 255, 255]
+        self.boss_pos = (220,320)
+
 
     def launch(self):
         while True:
@@ -23,9 +31,12 @@ class Main:
                     return 0
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     self.try_to_click(event.pos)
+                    self.try_to_click_boss(event.pos)
 
     def draw(self):
         self.boss_menu.draw_bloks(self.screen)
+        self.boss.draw_boss(self.screen)
+        self.boss.hp_boss(self.active_boss_level, self.screen)
         pygame.display.flip()
 
 
@@ -35,6 +46,13 @@ class Main:
                 if isinstance(blok, LevelChanger):
                     self.boss_menu.change_boss_info(blok.step)
                     break
+
+    def try_to_click_boss(self, pos):
+        if boss.is_clicked_boss(pos):
+            if isinstance(boss, Boss):
+                self.boss.atack(self.active_boss_level)
+                break
+
 
 main = Main()
 main.launch()
